b0db8ac5537724cd84cf3dce22dc28da0e48ce4e
irfanhsajid <irfanhsajid@gmail.com>
2021-09-02 19:58:39 +0600

    optional chaining added

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ searchBtn.addEventListener('click', function () {
 
     //fixing empty input error 
     if (search === "") {
-        errorDiv.innerHTML = ` <h3 class="text-center p-3 w-50 mx-auto rounded-3"><span class="text-danger my-2">Warning!! <br> </span>Search field can't be
-        Empty, Write something to see the result.
+        errorDiv.innerHTML = ` <h3 class="text-center p-3 w-50 mx-auto rounded-3"><span class="text-danger mb-2">Warning!! <br> </span> <span class ="text-muted">Search field can't be
+        Empty, Write something to see the result.</span>
     </h3> `;
         return;
     }
@@ -36,9 +36,9 @@ searchBtn.addEventListener('click', function () {
 const displayBooks = books => {
     const bookContainer = document.getElementById('book-container');
     bookContainer.innerHTML = '';
-    //fixing irravalent input error
+    //fixing irravalent Book Name error
     if (searchText.value !== 'books.title') {
-        invalidErrorDiv.innerHTML = ` <h3 class=" text-center p-3 w-50 mx-auto rounded-3"> <span class="text-danger my-2"> Irravalent Input <br> </span> Search Instead <span class= "text-success"> 'Smashing JavaScript,Python,java'</span>etc Names. 
+        invalidErrorDiv.innerHTML = ` <h3 class=" bg-secondary text-center p-3 w-50 mx-auto rounded-3 shadow-lg"> <span class="text-warning mb-3"> Irravalent Book Name !!<br> </span> Search Instead <span class= "text-light"> 'Python,Smashing JavaScript,Java'</span>etc. 
         </h3> `;
         //clear the parent div (items-div)
         searchResult.textContent = '';
@@ -47,26 +47,31 @@ const displayBooks = books => {
     }
 
     books.forEach(book => {
-        searchResult.innerHTML = ` <h4 class=" text-danger text-center p-3 w-50 mx-auto rounded-3"> ${books.length} Results Found for ${searchText.value} !!
-        </h4> `;
+        searchResult.innerHTML = ` <h3 class="text-danger text-center p-3 w-50 mx-auto"> ${books.length}<small class= "text-muted"> Results Found for ${searchText.value} !! </small>
+        </h3> `;
         //clear the irravalent input
         invalidErrorDiv.textContent = '';
         //creating book cards Dynamically
         const div = document.createElement("div");
-        div.classList.add('col-3', 'rounded-3', 'border', 'p-2', 'm-3', 'shadow-lg', 'overflow-hidden');
+        div.classList.add('col-md-3', 'col-12', 'rounded-3', 'border', 'p-2', 'm-3', 'shadow-lg', 'overflow-hidden');
         div.innerHTML = `
                <div class="rounded overflow-hidden p-2">
                                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"
-                                   class="w-100" alt="" />
+                                   class="w-100" alt="Images Not Found"/>
                            </div> 
                            <div class="py-2 text-center">
-                               <h5 class="fw-bolder" >${book.title}</h5>
-                               <p>Author: ${book.author_name}</p>
-                               <p>First Published in ${book.first_publish_year}</p>   
+                               <h6 class="fw-bold">${book.title}</h6>
+                               <p> <span class="text-info">Author:</span> ${book.author_name}</p>
+                               <p><span class="text-info">Publisher:</span> ${book.publisher?.[0] ?? 'Not found'}</p>
+                               <p>First Published in
+                               <span class="text-info">${book.first_publish_year}</span>
+                               </p>   
                            </div>
                `;
         bookContainer.appendChild(div);
+
     });
     //clear the input search field text
     searchText.value = '';
+
 }
